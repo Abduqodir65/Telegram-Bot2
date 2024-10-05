a60f379332b31c1eb653e2add2ab310d4b258e88
Abduqodir65 <abduqodirgithub@gmail.com>
2024-10-05 19:52:11 +0500

    fist commit

---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    token: process.env.TELEGRAM_TOKEN,
+    channelId: '@your_channel_id', 
+};
---
+++ src/database.js
@@ -0,0 +1,8 @@
+const videos = [
+    { id: 1, title: 'JavaScript Kirish', url: 'https://video-url1.com' },
+    { id: 2, title: 'Node.js Asoslari', url: 'https://video-url2.com' },
+];
+
+module.exports = {
+    getVideos: () => videos,
+};
